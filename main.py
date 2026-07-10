aea6800efae3c86f7c86c931c414ae809d146733
web安全 <178897973@qq.com>
2026-07-10 16:42:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 import time
-r=requests.get("https://8cb4eb4a046b49b29a9e1cef1f77ab20.api.mockbin.io/"+time.time())
+r=requests.get("https://8cb4eb4a046b49b29a9e1cef1f77ab20.api.mockbin.io/"+str(time.time()))
 print(r.text)
